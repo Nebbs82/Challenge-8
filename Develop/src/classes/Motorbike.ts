370b23e204ff3524087a1927cb3c8c26620e5ce4
Nebbs82 <nathan.r.ebbesen@hotmail.com>
2024-11-26 23:30:26 -0600

    Fixed the error with the motorbike wheels

--- Develop/src/classes/Motorbike.ts
+++ Develop/src/classes/Motorbike.ts
@@ -26,7 +26,7 @@ class Motorbike extends Vehicle {
     year: number,
     weight: number,
     topSpeed: number,
-    wheels: Wheel[]
+    wheels: Wheel[] = []
   ) {
     // TODO: The constructor should call the constructor of the parent class, Vehicle
     super();
